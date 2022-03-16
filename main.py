96071b9c2c660248d3aa758fe0294994bbdf1f18
RafaelCocoa <101624917+RafaelCocoa@users.noreply.github.com>
2022-03-16 15:11:16 +0100

    komplett verändert

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-
+while True:
+    if input.button_is_pressed(Button.A):
+        basic.show_number(10)
+        basic.pause(100)
+        basic.show_number(9)
+        basic.pause(100)
+        basic.show_number(8)
+        basic.pause(100)
+        basic.show_number(7)
+        basic.pause(100)
+        basic.show_number(6)
+        basic.pause(100)
+        basic.show_number(5)
+        basic.pause(100)
+        basic.show_number(4)
+        basic.pause(100)
+        basic.show_number(3)
+        basic.pause(100)
+        basic.show_number(2)
+        basic.pause(100)
+        basic.show_number(1)
+        basic.pause(100)
+        basic.show_number(0)
+        music.play_tone(Note.C, music.beat(100))
+        music.play_tone(Note.D, music.beat(100))
+        music.play_tone(Note.E, music.beat(100))
+        music.play_tone(Note.F, music.beat(100))
+        music.play_tone(Note.G, music.beat(100))
+        music.play_tone(Note.A, music.beat(100))
+        music.play_tone(Note.B, music.beat(100))
+        music.play_tone(Note.C5, music.beat(100))
+        basic.plot_leds("""
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        """)
+        basic.pause(1000)
+        basic.plot_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
--- main.ts
+++ main.ts
@@ -1,1 +1,49 @@
-
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(10)
+        basic.pause(100)
+        basic.showNumber(9)
+        basic.pause(100)
+        basic.showNumber(8)
+        basic.pause(100)
+        basic.showNumber(7)
+        basic.pause(100)
+        basic.showNumber(6)
+        basic.pause(100)
+        basic.showNumber(5)
+        basic.pause(100)
+        basic.showNumber(4)
+        basic.pause(100)
+        basic.showNumber(3)
+        basic.pause(100)
+        basic.showNumber(2)
+        basic.pause(100)
+        basic.showNumber(1)
+        basic.pause(100)
+        basic.showNumber(0)
+        music.playTone(Note.C, music.beat(100))
+        music.playTone(Note.D, music.beat(100))
+        music.playTone(Note.E, music.beat(100))
+        music.playTone(Note.F, music.beat(100))
+        music.playTone(Note.G, music.beat(100))
+        music.playTone(Note.A, music.beat(100))
+        music.playTone(Note.B, music.beat(100))
+        music.playTone(Note.C5, music.beat(100))
+        basic.plotLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+        basic.pause(1000)
+        basic.plotLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    }
+    
+}
